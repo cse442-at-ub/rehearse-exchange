256d83792ed662242bc5983942169b330f17f29e
fdpilkington <fdpilkington@gmail.com>
2020-02-16 16:12:25 -0500

    fixed PriceChart merge error

--- src/components/PriceChart.vue
+++ src/components/PriceChart.vue
@@ -84,18 +84,3 @@ ul#list li{
   margin:3rem;
 }
 </style>
-=======
-<template>
-<div></div>
-</template>
-
-<script>
-export default {
-  name: 'PriceChart'
-}
-</script>
-
-<style scoped>
-
-</style>
->>>>>>> 9addbcea2ebf98f64ab23932f6d629b5d82ea48c
