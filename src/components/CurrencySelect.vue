499f6b41cee3a4a4aec816a6b381cf17bb5292ad
louismot <31669216+louismot@users.noreply.github.com>
2020-02-17 10:37:25 -0500

    Add files via upload
    task #14

--- src/components/CurrencySelect.vue
+++ src/components/CurrencySelect.vue
@@ -1,13 +1,60 @@
 <template>
-<div></div>
+<div class="dropdown is-hoverable">
+  <div class="dropdown-trigger">
+    <button class="button" onclick = " " aria-haspopup="true" aria-controls="dropdown-menu2">
+      <span>{{selectedCurrency}}</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+    <div class="dropdown-content">
+<!-- Start of Content -->
+      <div class="dropdown-item">
+        <button><strong>BTC-USD</strong></button>
+
+      </div>
+      <hr class="dropdown-divider">
+     
+      <div class="dropdown-item">
+        <button><strong>BTC-ETH</strong></button>
+      </div>
+      <hr class="dropdown-divider">
+     
+      <div class="dropdown-item">
+        <button><strong>BTC-LINK</strong></button>
+      </div>
+      <hr class="dropdown-divider">
+     
+      <div class="dropdown-item">
+        <button><strong>ETH-USD</strong></button>
+      </div>
+      <hr class="dropdown-divider">
+     
+      <div class="dropdown-item">
+        <button><strong>ETH-LINK</strong></button>
+      </div>
+      <hr class="dropdown-divider">
+     
+      <div class="dropdown-item">
+        <button><strong>LINK-USD</strong></button>
+      </div>
+<!-- End of Content -->
+    </div>
+  </div>
+</div>
+
+
+
+  
 </template>
 
 <script>
 export default {
-  name: 'CurrencySelect'
+  name: 'CurrencySelect',
+  props: {
+    selectedCurrency: String
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
