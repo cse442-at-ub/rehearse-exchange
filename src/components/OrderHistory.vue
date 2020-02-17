b5ccf2519989e3eea3f97ffe26009b6dfe3264bd
louismot <31669216+louismot@users.noreply.github.com>
2020-02-17 10:20:03 -0500

    Add files via upload
    task #13

--- src/components/OrderHistory.vue
+++ src/components/OrderHistory.vue
@@ -1,13 +1,119 @@
 <template>
-<div></div>
+  <div class="table-container">
+
+    <table class="table is-bordered is-striped is-narrow is-haveable">
+      <thead>
+        <th>Fills</th>
+        <tr>
+          <th><abbr title="Type">Type</abbr></th>
+          <th><abbr title="Size">Size</abbr></th>
+          <th><abbr title="Price">Price</abbr></th>
+          <th><abbr title="Fee">Fee</abbr></th>
+          <th><abbr title="Time">Time</abbr></th>
+          <th><abbr title="Delete"></abbr></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Sell</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+
+        <tr>
+          <th>Buy</th>
+          <td>50.000000000</td>
+          <td>100.00</td>
+          <td>.55624</td>
+          <td>5h</td>
+          <td><button>x</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'OrderHistory'
+  name: 'OrderHistory',
+  props: {
+
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
