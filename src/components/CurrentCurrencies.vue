159458c1ea35e30e1fb7df0ce87461b634d354f7
Brandon <brandonhosken@gmail.com>
2020-02-21 14:39:21 -0500

    #30

--- src/components/CurrentCurrencies.vue
+++ src/components/CurrentCurrencies.vue
@@ -11,23 +11,23 @@
   <div class="dropdown-content">
     <div class="dropdown-item">
       <option>
-        <h3 class= "title is-3">BTC: <p class="walletAmount"></p></h3>
+        <h3 class= "title is-3">BTC: <p class="walletAmount">{{this.$parent.btcAmount}}</p></h3>
       </option>
        <hr class="dropdown-divider">
       <option>
-        <h3 class ="title is-3">ETH: <p class="walletAmount"></p></h3>
+        <h3 class ="title is-3">ETH: <p class="walletAmount">{{this.$parent.ethAmount}}</p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-        <h3 class ="title is-3">LTC: <p class="walletAmount"></p></h3>
+        <h3 class ="title is-3">LTC: <p class="walletAmount">{{this.$parent.ltcAmount}}</p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-      <h3 class="title is-3">XRP: <p class="walletAmount"></p></h3>
+      <h3 class="title is-3">XRP: <p class="walletAmount">{{this.$parent.xrpAmount}}</p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-        <h3 class="title is-3">LINK: <p class="walletAmount"></p></h3>
+        <h3 class="title is-3">LINK: <p class="walletAmount">{{this.$parent.linkAmount}}</p></h3>
       </option>
     </div>
   </div>
