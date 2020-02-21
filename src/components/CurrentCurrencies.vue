19e4a16c3a52e0b03ba45be0ef7dc10b9f085172
Brandon <brandonhosken@gmail.com>
2020-02-21 14:38:23 -0500

    #29

--- src/components/CurrentCurrencies.vue
+++ src/components/CurrentCurrencies.vue
@@ -3,6 +3,7 @@
 <div class="dropdown is-hoverable">
 <div class="dropdown-trigger">
   <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+
     <span style="text-align:center;">Wallet</span>
   </button>
 </div>
@@ -10,23 +11,23 @@
   <div class="dropdown-content">
     <div class="dropdown-item">
       <option>
-
+        <h3 class= "title is-3">BTC: <p class="walletAmount"></p></h3>
       </option>
        <hr class="dropdown-divider">
       <option>
-
+        <h3 class ="title is-3">ETH: <p class="walletAmount"></p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-
+        <h3 class ="title is-3">LTC: <p class="walletAmount"></p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-
+      <h3 class="title is-3">XRP: <p class="walletAmount"></p></h3>
       </option>
       <hr class="dropdown-divider">
       <option>
-
+        <h3 class="title is-3">LINK: <p class="walletAmount"></p></h3>
       </option>
     </div>
   </div>
