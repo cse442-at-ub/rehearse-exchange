0ebf39e9648538b1fdc5413f7885e897c403bcde
Brandon <brandonhosken@gmail.com>
2020-02-21 14:32:20 -0500

    #27

--- src/components/CurrentCurrencies.vue
+++ src/components/CurrentCurrencies.vue
@@ -1,1 +1,50 @@
+<template>
 
+<div class="dropdown is-hoverable">
+<div class="dropdown-trigger">
+  <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+    <span style="text-align:center;">Wallet</span>
+  </button>
+</div>
+<div class="dropdown-menu" id="dropdown-menu4" role="menu">
+  <div class="dropdown-content">
+    <div class="dropdown-item">
+      <option>
+
+      </option>
+       <hr class="dropdown-divider">
+      <option>
+
+      </option>
+      <hr class="dropdown-divider">
+      <option>
+
+      </option>
+      <hr class="dropdown-divider">
+      <option>
+
+      </option>
+      <hr class="dropdown-divider">
+      <option>
+
+      </option>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'CurrentCurrencies',
+
+}
+</script>
+<style>
+.walletAmount{
+  text-align:left;
+}
+
+</style>
