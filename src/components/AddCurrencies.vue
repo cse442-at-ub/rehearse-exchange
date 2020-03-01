b5a0dbfacf08e6ec29e5d881661858d89837ac55
Brandon <brandonhosken@gmail.com>
2020-03-01 15:04:40 -0500

    Added dropdown for task#49

--- src/components/AddCurrencies.vue
+++ src/components/AddCurrencies.vue
@@ -10,7 +10,35 @@
 <div class="dropdown-menu" id="dropdown-menu4" role="menu">
   <div class="dropdown-content">
     <div class="dropdown-item">
-
+      <div class="control">
+        <div class="select">
+          <select>
+            <option>
+              <button class="button">BTC</button>
+            </option>
+            <hr class="dropdown-divider">
+            <option>
+              <button class="button">ETH</button>
+            </option>
+            <hr class="dropdown-divider">
+            <option>
+              <button class="button">LTC</button>
+            </option>
+            <hr class="dropdown-divider">
+            <option>
+              <button class="button">XRP</button>
+            </option>
+            <hr class="dropdown-divider">
+            <option>
+              <button class="button">LINK</button>
+            </option>
+            <hr class="dropdown-divider">
+            <option>
+              <button class="button">USD</button>
+            </option>
+          </select>
+        </div>
+      </div>
     </div>
   </div>
 </div>
