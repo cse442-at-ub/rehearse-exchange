1a33ace8f7325a8cd9c3d8470fc4f56a52fe72c7
Brandon <brandonhosken@gmail.com>
2020-03-05 13:14:41 -0500

    Text field for currencies #48

--- src/components/AddCurrencies.vue
+++ src/components/AddCurrencies.vue
@@ -39,6 +39,16 @@
           </select>
         </div>
       </div>
+      <div class="field has-addons has-addons-centered">
+        <p class="control is-expanded">
+          <input class="input is-fullwidth" type="text" placeholder="0.00">
+        </p>
+        <p class="control">
+          <a class="button is-outlined is-static">
+            <span>Add</span>
+          </a>
+        </p>
+      </div>
     </div>
   </div>
 </div>
