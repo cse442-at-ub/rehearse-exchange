763f0626432f9780c89fe90c213cb138a944371f
louismot <louismot@buffalo.edu>
2020-04-16 10:52:29 -0400

    added emit for chosen timefram

--- src/components/PriceChart.vue
+++ src/components/PriceChart.vue
@@ -5,27 +5,27 @@
       <div class="control">
         <div class="select">
           <select>
-            <option>
+            <option @click = "getTime(1)">
               <button class="button">1m</button>
             </option>
             <hr class="dropdown-divider">
-            <option>
+            <option @click = "getTime(2)">
               <button class="button">5m</button>
             </option>
             <hr class="dropdown-divider">
-            <option>
+            <option @click = "getTime(3)">
               <button class="button">15m</button>
             </option>
             <hr class="dropdown-divider">
-            <option>
+            <option @click = "getTime(4)">
               <button class="button">1h</button>
             </option>
             <hr class="dropdown-divider">
-            <option>
+            <option @click = "getTime(5)">
               <button class="button">6h</button>
             </option>
             <hr class="dropdown-divider">
-            <option>
+            <option @click = "getTime(6)">
               <button class="button">1d</button>
             </option>
           </select>
@@ -45,7 +45,12 @@ export default {
   name:'PriceChart',
   components: {
     RenderPriceChart
-  }
+  },
+  methods:{
+    getTime(alpha) {
+      this.$emit('getTime', alpha);
+
+    }
 }
 </script>
 
